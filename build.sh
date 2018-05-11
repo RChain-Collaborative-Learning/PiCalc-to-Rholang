55622300b1ecc1880f3ecd13d9a764234326add2
Thomas Schoffelen <thomas@includable.com>
2018-05-11 17:19:13 +0200

    Pandoc in APT is outdated, so we download a newer release

--- build.sh
+++ build.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+wget "https://s3-us-west-1.amazonaws.com/pandoc-releases/pandoc"
+chmod +x ./pandoc
+
 for file in lesson*.md; do
   name=${file##*/}
   base=${name%.md}
-  pandoc --standalone --katex --to revealjs --slide-level 2 --output "$base.html" "$file"
+  ./pandoc --standalone --katex --to revealjs --slide-level 2 --output "$base.html" "$file"
 done
